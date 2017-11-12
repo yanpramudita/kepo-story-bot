46a764f69f396b276e9fa112af6eea3489fa83a8
yanpramudita <rahadyan.pramudita@hotmail.com>
2017-11-13 03:00:59 +0700

    add error handler on web controller

--- controllers/web.js
+++ controllers/web.js
@@ -1,19 +1,64 @@
 const express = require('express');
-const router = express.Router();
+const _ = require('lodash');
+const Bluebird = require('bluebird');
+
 const instagramAgent = require('../utils/instagram_agent');
 
-router.get('/user/get/:username', function(req, res) {
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  res.send('<h1>Hello, world!</h1>');
+});
+
+router.get('/get/:username/details', function(req, res) {
   instagramAgent.getUserID(req.params.username)
     .then((user) => {
+      if(_.isUndefined(user)){
+        return Bluebird.reject(404);
+      }
       res.json(user)
+    })
+    .catch((err) => {
+      if(err == 404){
+        return sendNotFound(req, res);
+      }
+      return sendError(req, res, err);
     });
 });
 
-router.get('/stories/get/:user_id', function(req, res) {
-  instagramAgent.getStories(req.params.user_id)
+router.get('/get/:username/stories', function(req, res) {
+  instagramAgent.getUserID(req.params.username)
+    .then((user) => {
+      if(_.isUndefined(user)){
+        return Bluebird.reject(404);
+      }
+      res.json(user)
+    })
+    .then((user_id) => instagramAgent.getStories(user_id))
     .then((stories) => {
       res.json(stories)
+    })
+    .catch((err) => {
+      if(err == 404){
+        return sendNotFound(req, res);
+      }
+      return sendError(req, res);
     });
 });
+
+function sendNotFound(req, res) {
+  return res.status(404).json({
+    status: 404,
+    message: "Not Found"
+  });
+}
+
+function sendError(req, res, err) {
+  console.error(err);
+  return res.status(500).json({
+    status: 500,
+    message: "Internal Server Error"
+  });
+}
 
 module.exports = router;
